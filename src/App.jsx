8e3881c7e91006d160c2e9241ff64d919121fd3a
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-04 13:59:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-red-500">
+      <Navbar />
+      <Hero />
+    </div>
+  );
 };
 
 export default App;
