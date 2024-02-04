578ad03e6b49ea91259f776f403eb16c761fc5eb
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-04 14:40:56 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,34 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   return (
     <div className="">
       <div className="container">
-        <div className=""></div>
+        <div className="">
+          <h1 className="uppercase">
+            Straw<span className="">Berries</span>
+          </h1>
+          <ul className="hidden lg:flex space-x-14">
+            <li className="">
+              <a className="" href="#">
+                Home
+              </a>
+            </li>
+            <li className="">
+              <a className="" href="#">
+                Where to find
+              </a>
+            </li>
+            <li className="">
+              <a className="" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <GiHamburgerMenu />
       </div>
     </div>
   );
