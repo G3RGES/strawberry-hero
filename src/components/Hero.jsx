c275f5cec4a55cd8b9a14d52acadb9eddd2e6fec
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-05 10:52:52 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import Navbar from "./Navbar";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <main className="bg-primaryDark md:py-6">
+      <section
+        className="relative min-h-[650px] bg-gradient-to-r from-primary
+      to-secondary w-full md:rounded-xl shadow-md "
+      >
+        <div className="container">
+          <Navbar />
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Hero;
