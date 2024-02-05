9038fbe22b55ce805e3245c32adf46ecefc782b9
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-05 11:04:18 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,14 @@ import Navbar from "./Navbar";
 
 const Hero = () => {
   return (
-    <main className="bg-primaryDark md:py-6">
+    <main className="bg-primaryDark md:py-6 md:px-12">
       <section
         className="relative min-h-[650px] bg-gradient-to-r from-primary
       to-secondary w-full md:rounded-xl shadow-md "
       >
         <div className="container">
           <Navbar />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"></div>
         </div>
       </section>
     </main>
