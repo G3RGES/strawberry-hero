ce28f87892e47c33c3334829641eecbf9029121f
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-07 11:28:37 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { GiHamburgerMenu } from "react-icons/gi";
 const Navbar = ({ setSidebar, sidebar }) => {
   return (
     <div className="text-white py-2 absolute top-0 left-0 w-full z-20">
-      <div className="container ">
+      <div data-aos="fade-down" className="container ">
         <div className="flex justify-between items-center">
           <h1 className="uppercase text-4xl font-bold">
             Straw<span className="font-normal">Berries</span>
