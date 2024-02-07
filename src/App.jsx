1e0632c4f885ed047cd34de61c6134273fee17ef
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-07 10:46:01 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect } from "react";
+
 // import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 
+// AOS
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const App = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
     <div className="">
       {/* <Navbar /> */}
